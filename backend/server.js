5895bf87c6bc74898efd91eb2ed0f1a8a6778c99
Kaushik <your-email@example.com>
2025-07-01 20:19:20 +0530

    server change

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,9 @@ app.use("/api/students", router);
 app.use("/api/admin", adminRouter)
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));  
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));  
+// })
 
 
 const PORT = process.env.PORT || 8000;

6dac2e5bf5577e3abc255b8210956ddc7bf8cd96
Kaushik <your-email@example.com>
2025-07-03 02:00:07 +0530

    build added

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -121,20 +121,22 @@ function Admin() {
                         />
                     ) : (
                         <>
-                        <div className='bg-white/35 backdrop-blur-xs border border-[#920F05] w-full px-3 py-2.5 rounded-2xl text-left'>
-                            <p><strong>Name:</strong> {studentData.name}</p>
-                            <p><strong>Roll:</strong> {studentData.college_roll}</p>
-                            <p><strong>Year:</strong> {studentData.year}</p>
-                            <p><strong>Semester:</strong> {studentData.sem}</p>
-                            <p><strong>Food Preference:</strong> {studentData.foodType}</p>
-                        </div>
+                            <div className='bg-white/25 backdrop-blur-xs border border-[#920F05] w-full max-w-md mx-auto px-4 py-3 rounded-2xl text-left'>
+                                <p><strong>Name:</strong> {studentData.name}</p>
+                                <p><strong>Roll:</strong> {studentData.college_roll}</p>
+                                <p><strong>Year:</strong> {studentData.year}</p>
+                                <p><strong>Semester:</strong> {studentData.sem}</p>
+                                <p><strong>Food Preference:</strong> {studentData.foodType}</p>
+                            </div>
+
                             <button
-                                className="mt-4 px-6 py-2 text-2xl rounded-xl bg-[#D9D9D9] border border-[#920F05] text-[#920F05] transition duration-300 shadow-[2px_2px_4px_rgba(0,0,0,0.2)] tracking-wide eb-garamond-semibold"
+                                className="mt-6 px-8 py-3 text-2xl rounded-xl bg-[#D9D9D9] border border-[#920F05] text-[#920F05] transition duration-300 shadow-[2px_2px_4px_rgba(0,0,0,0.2)] tracking-wide eb-garamond-semibold"
                                 onClick={toggleQRData}
                             >
-                                OK
+                                Done
                             </button>
-                        
+
+
                         </>
                     )}
                 </div>

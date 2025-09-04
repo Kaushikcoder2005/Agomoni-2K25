55674b5439fe67fefe90eb1f1bb71464bc0565bc
Kaushik <your-email@example.com>
2025-09-04 19:20:14 +0530

    sem added

--- frontend/src/pages/ShowQRcode.jsx
+++ frontend/src/pages/ShowQRcode.jsx
@@ -127,6 +127,8 @@ function ShowQRcode() {
                             <option value="4">4</option>
                             <option value="5">5</option>
                             <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
                         </select>
                     </div>
 
